c6a42c705fbe6a0c3144cfe697274ac0fa853e3f
codekult <codekult@gmail.com>
2025-11-26 15:29:06 -0300

    fix text align

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,27 +14,27 @@ export default function Home() {
       </div>
       <main>
         <article className="flex flex-col justify-center items-center mt-5">
-          <p className="mb-3">
+          <p className="mb-3 text-center">
             <b>Senior Full-Stack Engineer</b> with <b>+10 years</b> of
             experience delivering complex user-centric web systems at scale.
             Expert in bridging the gap between <b>Product</b> and{" "}
             <b>Platform</b> engineering to build resilient, high- availability
             solutions for global industry leaders and ambitious startups
           </p>
-          <p className="mb-3">
+          <p className="mb-3 text-center">
             <b>Currently exploring</b> how to leverage my extensive expertise in
             distributed systems and performance optimization to build
             production-grade <b>Applied AI</b> solutions, specializing in{" "}
             <b>MCP Servers</b>, <b>Agentic Workflows</b>, and{" "}
             <b>LLM Integration</b>.
           </p>
-          <p className="mb-3">
+          <p className="mb-3 text-center">
             Operating remotely from the Sierras of Córdoba, Argentina.{" "}
             <b>High-Availability</b> & <b>Deep Focus</b> shape my work. When
             AFK, I embrace a low-tech, digital minimalist, outdoor enthusiast
             lifestyle.
           </p>
-          <p>
+          <p className="text-center">
             <b>Dark Mode</b> always <b>On</b>.
           </p>
         </article>
